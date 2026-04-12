8ce09daaed3be113426cdd3506438f22344d44e2
Hat-Master-PocketEdition <agwatkins04@gmail.com>
2026-04-11 20:33:33 -0400

    Initial Commit

---
+++ ConnectFour_Group1/ConnectFour_Group1/Form1.cs
@@ -0,0 +1,19 @@
+namespace ConnectFour_Group1
+{
+    public partial class Form1 : Form
+    {
+
+        int[,] gameBoard = new int[7, 6];
+        //comment
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawBoard()
+        {
+
+        }
+    }
+}
