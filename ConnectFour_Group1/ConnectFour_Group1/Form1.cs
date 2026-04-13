2f06ada17e0f77d0ae790bef6c1908456279964d
Sgt. Ahab <198687949+Sgt-Ahab@users.noreply.github.com>
2026-04-12 21:59:32 -0400

    Comment Block 1 - Form1
    Made a Comment Block to gather thoughts, state some code etiquettes, and discussion of Form Beauty Standard.

--- ConnectFour_Group1/ConnectFour_Group1/Form1.cs
+++ ConnectFour_Group1/ConnectFour_Group1/Form1.cs
@@ -5,7 +5,23 @@ namespace ConnectFour_Group1
 
         int[,] gameBoard = new int[7, 6];
         //comment test
-
+        //Comment Board for Form 1 Start - DS
+        //I will sign off my Comment Blocks with my initials
+        //Important we discuss program clocking procedures.
+        //Either a Clock-In/Out system in Discord, allowing our group to know where and when.
+        //Or, agreed timeframes on when working on the Code.
+        //Once procedures are finalize, we can replace this comment, and remove line 10-12 (and others obsoleted).
+        //As soon as we reach consensus, it is off to the races with getting this done.
+        //====================================================
+        //FORM BEAUTY STANDARD
+        //====================================================
+        //Details for Front-End, form style, font sizes and type. I will ask in the meeting.
+        //Name cases for Items (Buttons, Labels, TextBoxes, etc.)
+        //====================================================
+        //We will need to make the README to gather our work, and make it effective.
+        //Any other pieces we can muster for the coding etiquettes for our project.
+        //Looking forward to coding together, team. Good luck, good coding.
+        //End Comment Block - DS
         public Form1()
         {
             InitializeComponent();
